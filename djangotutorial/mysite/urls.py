1665bf67cc4e5bb3ac564420d20aead02f4c4552
Sahilms48 <ssarnaik48@gmail.com>
2025-09-29 13:15:46 -0400

    Django app created

---
+++ djangotutorial/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    # Add a redirect from root to polls
+    path('', RedirectView.as_view(url='/polls/', permanent=True)),
+]
